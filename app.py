59227054ff25d5cef14bf7eaad1ebcd82f359828
supertarmp21 <penguindad.no2@gmail.com>
2024-02-25 13:57:10 +1300

    bay timesheet load working

--- app.py
+++ app.py
@@ -1,21 +1,40 @@
 from flask import Flask, render_template, jsonify
-from database import get_bays
+from database import get_bays, get_timeslots, get_bookings
+import datetime
 
 app = Flask(__name__)
 
 
+@app.context_processor
+def inject_today_date():
+    return {'today_date': datetime.date.today()}
+
+
+def load_bookings(bookingDate, bayID):
+    bookinglist = get_bookings(bookingDate=bookingDate, bayID=bayID)
+    return bookinglist
+app.jinja_env.globals['load_bookings']=load_bookings
+
+
 @app.route("/")
-def kgolf_booking():
-    bays = get_bays()  
+def kgolf_bookingPage():
+    bays = get_bays()      
+    timeslots = get_timeslots()
     return render_template('home.html', 
                            bays=bays,
+                           timeslots=timeslots,
+                           bookings=None,
                            company_name='KGOLF')
 
-
 @app.route("/api/bays")
-def list_jobs():
+def list_bays():
     bays = get_bays()
     return jsonify(bays)
+
+@app.route("/api/timeslots")
+def list_timeslots():
+    timeslots = get_timeslots()
+    return jsonify(timeslots)
 
 
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine, text
 import os
+import datetime
 
 db_connection_string = os.environ['DB_CONNECTION_STRING']
 
@@ -11,14 +12,72 @@ engine = create_engine(
         }
     })
 
-# with engine.connect() as conn:
-#     result = conn.execute(text("select * from Bay"))
-    
-#     result_dict = [b._asdict() for b in result.all()]    
-#     print(result_dict)
-
 def get_bays():
     with engine.connect() as conn:
-        result = conn.execute(text("select * from Bay"))        
+        result = conn.execute(text("SELECT * FROM Bay WHERE IsActive = 1"))        
         bays = [b._asdict() for b in result.all()]  
         return bays
+    
+def get_timeslots():
+    query = "SELECT "\
+	        "   *,"\
+            "   time_format(TS.TimeValue, '%l %p') AS `HourNameDisplay`"\
+            "FROM kgolf.Timeslot TS"
+    with engine.connect() as conn:
+        result = conn.execute(text(query))
+        timeslots = [ts._asdict() for ts in result.all()]
+        return timeslots
+    
+def get_bookings(bookingDate, bayID):
+    query =  "SELECT"\
+            " 	TS.*,"\
+            "     time_format(TS.TimeValue, '%l:%i %p') AS `HourNameDisplay`,"\
+            "     CASE"\
+            " 		WHEN lag(Bookings.BookingID) OVER (partition by Bookings.BookingID order by TS.ID ASC) IS NULL AND Bookings.BookingID IS NOT NULL THEN"\
+            " 			'Start'"\
+            " 		WHEN lead(Bookings.BookingID) OVER (partition by Bookings.BookingID order by TS.ID ASC) IS NULL AND Bookings.BookingID IS NOT NULL THEN"\
+            " 			'End'"\
+            " 		WHEN lag(Bookings.BookingID) OVER (partition by Bookings.BookingID order by TS.ID ASC) = lead(Bookings.BookingID) OVER (partition by Bookings.BookingID order by TS.ID ASC) THEN"\
+            " 			'Between'"\
+            " 		ELSE"\
+            " 			NULL"\
+            " 	  END AS `BookingTimePoint`,"\
+            "     Bookings.*"\
+            " FROM Timeslot TS"\
+            " LEFT JOIN LATERAL ("\
+            " 	SELECT"\
+            " 		BH.ID As `BookingID`,"\
+            "         BH.BookingDate,"\
+            "         BH.BayID,"\
+            "         BH.BookingTypeID,"\
+            "         BH.CustomerID,"\
+            "         IFNULL(BH.BookingDescription,'') AS `BookingDescription`,"\
+            "         BH.IP,"\
+            "         BH.IsActive AS `IsBookingActive`,"\
+            "         BH.IsCancelled AS `IsBookingCancelled`,"\
+            "         BH.IsPaid,"\
+            "         BTD.IsBufferTime,"\
+            "         BT.Name AS `BookingTypeName`,"\
+            "         BT.Description AS `BookingTypeDescription`"\
+            "     FROM BookingHeader BH"\
+            "     INNER JOIN BookingTimeDetail BTD"\
+            " 		ON BH.ID = BTD.BookingHeaderID"\
+            " 	INNER JOIN BookingType BT"\
+            " 		ON BH.BookingTypeID = BT.ID"\
+            " 	INNER JOIN Bay Bay"\
+            " 		ON Bay.ID = BH.BayID"\
+            " 	WHERE"\
+            " 		TS.ID = BTD.TimeslotID"\
+            "         AND BH.IsActive = 1"\
+            " 		AND BH.BookingDate = '" + bookingDate.strftime('%Y-%m-%d') +"'"\
+            " 		AND BH.BayID = " + str(bayID) + \
+            "         AND BTD.IsActive = 1"\
+            " ) Bookings ON 1=1"\
+            " ORDER BY"\
+            " 	TS.ID ASC"    
+    with engine.connect() as conn:
+        result = conn.execute(text(query))
+        bookings = [bk._asdict() for bk in result.all()]
+        return bookings        
+
+
