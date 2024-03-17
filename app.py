1c9fd575271ef5cc6beb00739aa175b54614a92b
supertarmp21 <penguindad.no2@gmail.com>
2024-03-18 02:11:16 +1300

    submit works

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
-from flask import Flask, render_template, render_template_string, jsonify
-from database import get_bays, get_timeslots, get_bookings, get_availableStartTimeslots, get_availableEndTimeslots
+from flask import Flask, render_template, render_template_string, jsonify, request
+from database import get_bays, get_timeslots, get_bookings, get_availableStartTimeslots, get_availableEndTimeslots, insert_Booking, get_bookingType
 from datetime import datetime, date, timedelta
 from dateutil.relativedelta import relativedelta
 from htmlcalendar import htmlcalendar
+from htmx_flask import Htmx, request
 
+htmx = Htmx()
 app = Flask(__name__)
+htmx.init_app(app)
 
 date_format = '%Y-%m-%d'
 dateBooking = date.today()
 app.jinja_env.globals['dateBooking']=dateBooking
 app.jinja_env.globals['bayID'] = 1
 timeslots = get_timeslots() 
+bookingTypes = get_bookingType()
+
 
 @app.context_processor
 def inject_today_date():
@@ -26,9 +31,32 @@ def load_bookings(bookingDate, bayID):
     return bookinglist
 app.jinja_env.globals['load_bookings']=load_bookings
 
+def css_today_class():
+    todayclasses = ['today']
+    return todayclasses
+
+def load_calendar(startdate):
+    # app.jinja_env.globals['dateBooking']=dateBooking
+    # app.jinja_env.globals['bayID'] = 1    
+    # print(app.jinja_env.globals['dateBooking'])
+    # print("---")
+    # print(app.jinja_env.globals['bayID'])
+    class_today = css_today_class()    
+    return htmlcalendar(startdate, 
+                        months=1,
+                        #links=links,
+                        #classes=css_class,
+                        classes_today=class_today,
+                        currentSelectedDate=app.jinja_env.globals['dateBooking'],
+                        table_id="booking-calendar",
+                        show_year=True,
+                        show_month=True,
+                        td_custom_attribute_string='id="[[date]]" onclick="selectNewDate(this)" hx-post="/timesheet/[[date]]/" hx-target="#timesheet" hx-indicator="#timesheet-loadingbar" hx-swap="innerHTML show:bottom"')
+app.jinja_env.globals['load_calendar']=load_calendar
+
 
 @app.route('/timesheet/<strbookingdate>/<bayID>', methods=['POST'])
-def refresh_timesheet(strbookingdate, bayID):        
+def refresh_timesheet(strbookingdate, bayID):            
     dateBooking = datetime.strptime(strbookingdate, date_format).date()
     app.jinja_env.globals['dateBooking'] = dateBooking
     app.jinja_env.globals['bayID'] = bayID
@@ -57,32 +85,78 @@ def load_new_booking_form(strbookingdate, bayID, timeslotID):
                                   availableEndTimeslots=availableEndTimeslots,
                                   timeslots=timeslots)
 
-def css_today_class():
-    todayclasses = ['today']
-    return todayclasses
+@app.route('/get/endtimes/<strbookingdate>/<bayID>', methods=['POST']) #selected Starttime slot info can be found in the HTMX request header
+def get_availableEndTimes(strbookingdate, bayID):
+    templ ="""
+    {% for et in endtimes %}
+        <option value="{{ }}">{{ }}</option>
+    {% endfor %}
+    """
+    #selectedStartimeSlotID = request.htmx.
+    endtimes = None #TODO:
+    return render_template_string(templ, endtimes=endtimes)
 
-def load_calendar(startdate):
-    # app.jinja_env.globals['dateBooking']=dateBooking
-    # app.jinja_env.globals['bayID'] = 1    
-    # print(app.jinja_env.globals['dateBooking'])
-    # print("---")
-    # print(app.jinja_env.globals['bayID'])
-    class_today = css_today_class()    
-    return htmlcalendar(startdate, 
-                        months=1,
-                        #links=links,
-                        #classes=css_class,
-                        classes_today=class_today,
-                        currentSelectedDate=app.jinja_env.globals['dateBooking'],
-                        table_id="booking-calendar",
-                        show_year=True,
-                        show_month=True,
-                        td_custom_attribute_string='id="[[date]]" onclick="selectNewDate(this)" hx-post="/timesheet/[[date]]/" hx-target="#timesheet" hx-indicator="#timesheet-loadingbar" hx-swap="innerHTML show:bottom"')
-app.jinja_env.globals['load_calendar']=load_calendar
+
+@app.route('/book', methods=['POST'])
+def submit_booking():
+    if request.method == 'POST':
+        bookingDateSubmitted = datetime.strptime(request.form['dateBooking'], date_format).date()        
+        bookingBayIDSubmitted = int(request.form['bookingBayID'])
+        bookingTypeID = int(request.form['bookingType'])
+        bookingStartTimeSlotID = int(request.form['floatingInputStartTime'])
+        bookingEndTimeSlotID = int(request.form['floatingInputEndTime'])
+        bookingFirstname = request.form['floatingInputFirstname']
+        bookingLastname = request.form['floatingInputLastname']
+        bookingEmail = request.form['floatingInputEmail']
+        bookingPhone = request.form['floatingInputMobileNumber']
+        bookingPassword = request.form['floatingInputPassword']
+
+
+
+        bool_insert_result =  insert_Booking(bookingDateSubmitted,
+                                             bookingBayIDSubmitted,
+                                             bookingTypeID,
+                                             bookingStartTimeSlotID,
+                                             bookingEndTimeSlotID,
+                                             bookingFirstname,
+                                             bookingLastname,
+                                             bookingEmail,
+                                             bookingPhone,
+                                             bookingPassword)
+        
+        if bool_insert_result['result'] == True:
+            bookingDuration_in_minute = (bookingEndTimeSlotID - bookingStartTimeSlotID) * 15
+            bookingDuration_hourpart = int(bookingDuration_in_minute / 60)
+            bookingDuration_minutepart = bookingDuration_in_minute % 60
+            bays = get_bays() 
+            bookingTypeName = next(x for x in bookingTypes if x.get("ID") == bookingTypeID).get("Name")
+            
+
+            templ ="""
+                {% include 'booking_confirmation.html' %}    
+            """
+            return render_template_string(templ, 
+                                          bookingDateSubmitted=bookingDateSubmitted, 
+                                          bookingBayID=bookingBayIDSubmitted,
+                                          bookingBayName=next(x for x in bays if x.get("ID") == bookingBayIDSubmitted).get("BayName"), 
+                                          bookingBayDesc=next(x for x in bays if x.get("ID") == bookingBayIDSubmitted).get("BayDescription"),
+                                          bookingTypeName=bookingTypeName,
+                                          bookingStartTimeText=next(x for x in timeslots if x.get("ID") == bookingStartTimeSlotID).get("HourMinuteNameDisplay"),
+                                          bookingEndTimeText=next(x for x in timeslots if x.get("ID") == bookingEndTimeSlotID).get("HourMinuteNameDisplay"), 
+                                          bookingDuration_hourpart=bookingDuration_hourpart,
+                                          bookingDuration_minutepart=bookingDuration_minutepart,
+                                          bookingFullname=bookingFirstname + ' ' + bookingLastname,
+                                          bookingEmail=bookingEmail, 
+                                          bookingPhone=bookingPhone)
+        else:
+            return '''
+                <h3>Failed making a new booking</h3>
+            '''
 
 
 @app.route("/")
 def kgolf_bookingPage():    
+
     bays = get_bays()      
     timeslots = get_timeslots()
     bookinglist = get_bookings(bookingDate=date.today(), bayID=1)    
--- database.py
+++ database.py
@@ -1,7 +1,17 @@
-from sqlalchemy import create_engine, text
 import os
 import datetime
+from sqlalchemy import BindParameter, cast, create_engine, text, insert, MetaData, Table, Column
+import uuid
+import binascii
+from Crypto.Cipher import AES
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy import TypeDecorator
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import Session
+ 
 
+# DB stuff
 
 db_connection_string = os.environ['DB_CONNECTION_STRING']
 
@@ -19,10 +29,17 @@ def get_bays():
         bays = [b._asdict() for b in result.all()]  
         return bays
     
+def get_bookingType():
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT * FROM BookingType WHERE IsActive = 1"))        
+        bookingTypes = [b._asdict() for b in result.all()]  
+        return bookingTypes   
+    
 def get_timeslots():
     query = "SELECT "\
 	        "   *,"\
-            "   time_format(TS.TimeValue, '%l %p') AS `HourNameDisplay`"\
+            "   time_format(TS.TimeValue, '%l %p') AS `HourNameDisplay`,"\
+            "   time_format(TS.TimeValue, '%l:%i %p') AS `HourMinuteNameDisplay`"\
             "FROM kgolf.Timeslot TS"
     with engine.connect() as conn:
         result = conn.execute(text(query))
@@ -165,3 +182,170 @@ def get_availableEndTimeslots(strBookingDate, bayID):
         result = conn.execute(text(query))
         availableTimeslots = [ts._asdict() for ts in result.all()]
         return availableTimeslots              
+    
+def insert_Booking(bookingDate, bayID, bookingTypeID, startTimeslotID, endTimeslotID, custFirstname, custLastname, custEmail, custPhone, bookingPassword):
+    #Validate the booking details once again    
+    result_dict = {}    
+    #1. ensure bookingDate is today or future
+    if bookingDate < datetime.date.today():        
+        result_dict = { 'result': False, 'message': 'Booking date must be today or later.'}
+        return result_dict
+    #2. booking bayID is valid    
+    if isValidBay(bayID) == False:
+        result_dict = {'result': False, 'message': 'Invalid bay is selected.'}
+        return result_dict
+    #3. booking type is valid
+    if isValidBookingType(bookingTypeID) == False:
+        result_dict = {'result': False, 'message': 'Invalid booking type is selected.'}
+        return result_dict        
+    #3. booking slotsIDs are valid
+    if isValidTimeslot(startTimeslotID) == False or isValidTimeslot(endTimeslotID) == False:
+        result_dict = {'result': False, 'message': 'Invalid timeslot is selected.'}
+        return result_dict
+    if endTimeslotID - startTimeslotID < 2:
+        result_dict = {'result': False, 'message': 'booking session must be at least 30 minutes.'}
+        return result_dict        
+    #4. Check if booking date/bay/slots are available for a new booking.
+    if isBookingAvailable(bookingDate, bayID, startTimeslotID, endTimeslotID) == False:
+        result_dict = {'result': False, 'message': 'Selected Bay, Date and time is no longer available.'}
+        return result_dict
+    #5. Ensure Customer info is sufficient
+    if custEmail is None or str(custEmail).strip() == '':
+        result_dict = {'result': False, 'message': 'Email must be provided for booking.'}
+        return result_dict
+    if custPhone is None or str(custPhone).strip() == '' or len(str(custPhone).strip()) < 9:
+        result_dict = {'result': False, 'message': 'A valid phone number must be provided for booking.'}
+        return result_dict
+    if bookingPassword is None or bookingPassword.strip() == '':
+        result_dict = {'result': False, 'message': 'Booking password is required for you to manage your booking. Otherwise you will not be able to modify or cancel your booking.'}
+        return result_dict
+    
+    # All validation steps has been passed . so let's insert a new booking
+    failed_result_dict = {'result': False, 'message': 'Sorry, there was an error processing your booking request. Please try later again or contact by phone if the issue persists.'}
+    #1. Add customer
+    customerID = AddCustomer(str(custFirstname).strip(), str(custLastname).strip(), str(custEmail).strip(), str(custPhone).strip(), str(bookingPassword))
+    if customerID is None or customerID < 0:        
+        return failed_result_dict            
+    #2. Add Booking Header
+    bookingID = AddBookingHeader(bookingDate, bayID, bookingTypeID, customerID)
+    if bookingID is None or bookingID < 0:        
+        return failed_result_dict    
+    #3. Add BookingTime Detail
+    isDetailsAdded = AddBookingTimeDetails(bookingID, startTimeslotID, endTimeslotID)
+    if isDetailsAdded==True:
+        return {'result': True, 'message': 'successfully booked'}
+    else:
+        return failed_result_dict
+    
+
+def isValidBay(bayID):
+    return True
+
+def isValidBookingType(bookingTypeID):
+    return True
+
+def isValidTimeslot(timeslotID):
+    return True
+
+def isBookingAvailable(bookingDate, bayID, startTimeslotID, endTimeslotID):
+    return True
+
+
+def AddCustomer(custFirstname, custLastname, custEmail, custPhone, custPassword):        
+    query = text("\
+            INSERT INTO \
+                Customer \
+                (FirstName, LastName, FullName, Mobile, MobileNumericOnly, Email, PW, IsActive, Created, LastUpdated) \
+            VALUES \
+                (:FirstName, :LastName, :FullName, :Mobile, :MobileNumericOnly, :Email, AES_ENCRYPT(:PW, :MyEncryptKey), :IsActive, :Created, :LastUpdated)")                
+            
+    with engine.connect() as conn:
+        result = conn.execute(query,
+                              parameters=dict(FirstName=custFirstname,
+                                            LastName=custLastname,
+                                            FullName=(custFirstname + ' ' + custLastname),
+                                            Mobile=custPhone,
+                                            MobileNumericOnly=str(custPhone).lstrip('0'),
+                                            Email=custEmail,
+                                            PW=custPassword,
+                                            MyEncryptKey=custPhone,
+                                            IsActive=1,
+                                            Created=datetime.datetime.now(),
+                                            LastUpdated=datetime.datetime.now()))
+        
+        if result.lastrowid is not None and result.lastrowid > 0:
+            conn.commit()  
+            return result.lastrowid                  
+        else:
+            conn.rollback()
+            return None
+        
+
+
+def AddBookingHeader(bookingDate, bayID, bookingTypeID, customerID):
+    with engine.connect() as conn:                
+        conn_metaData = MetaData()
+        bookingHeaderTable = Table("BookingHeader", conn_metaData, autoload_with=conn)
+        insertStmt = insert(bookingHeaderTable).values(BookingDate=bookingDate,
+                                            BayID=bayID,
+                                            BookingTypeID=bookingTypeID,
+                                            CustomerID=customerID,                                            
+                                            IsActive=1,
+                                            IsCancelled=0,
+                                            IsPaid=0,
+                                            Created=datetime.datetime.now(),
+                                            LastUpdated=datetime.datetime.now())        
+        result = conn.execute(insertStmt)
+        if result.lastrowid is not None and result.lastrowid > 0:
+            conn.commit()  
+            return result.lastrowid                  
+        else:
+            conn.rollback()
+            return None    
+        
+def AddBookingTimeDetails(bookingHeaderID, startTimeslotID, endTimeslotID):
+    with engine.connect() as conn:
+        conn_metaData = MetaData()
+        timeDetailTable = Table("BookingTimeDetail", conn_metaData, autoload_with=conn)  
+        insertStmt = insert(timeDetailTable).values(
+            [{'BookingHeaderID': bookingHeaderID,'TimeslotID': tid,'IsActive': 1, 'IsBufferTime': 0} for tid in range(startTimeslotID, endTimeslotID + 1)]
+        )
+        result = conn.execute(insertStmt)
+        if result.lastrowid is not None and result.lastrowid > 0:
+            conn.commit()
+            return True
+        else:
+            conn.rollback()
+            return None
+
+
+
+# AES Encryption Decryption
+key = uuid.uuid4().bytes
+"""The encryption key.   Random for this example."""
+
+
+nonce = uuid.uuid4().bytes
+"""for WHERE criteria to work, we need the encrypted value to be the same
+each time, so use a fixed nonce if we need that feature.
+"""
+
+def aes_encrypt(data):
+    cipher = AES.new(key, AES.MODE_EAX, nonce=nonce)
+    data = data + (" " * (16 - (len(data) % 16)))
+    return cipher.encrypt(data.encode("utf-8")).hex()
+
+
+def aes_decrypt(data):
+    cipher = AES.new(key, AES.MODE_EAX, nonce=nonce)
+    return cipher.decrypt(binascii.unhexlify(data)).decode("utf-8").rstrip()
+
+
+class EncryptedValue(TypeDecorator):
+    impl = String
+
+    def process_bind_param(self, value, dialect):
+        return aes_encrypt(value)
+
+    def process_result_value(self, value, dialect):
+        return aes_decrypt(value)        
